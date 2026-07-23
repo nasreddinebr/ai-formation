2196c87865ab3ee765523994a8242afcc9bc6407
Nasreddine Berrached <nasreddine.be@hotmail.fr>
2026-07-23 21:17:18 +0200

    fix: add suppressHydrationWarning to body tag to prevent browser extension hydration mismatch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
